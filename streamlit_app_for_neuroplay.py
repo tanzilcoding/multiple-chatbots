1ee4f52c5b5b41512dccb6f77319fab523247ce4
tanzilcoding <89991847+tanzilcoding@users.noreply.github.com>
2023-09-09 18:00:56 +0600

    Create streamlit_app_for_neuroplay.py

---
+++ streamlit_app_for_neuroplay.py
@@ -0,0 +1,170 @@
+import os
+import time
+import openai
+import pinecone
+import streamlit as st
+from streamlit_chat import message
+from langchain.vectorstores import Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
+try:
+    import environment_variables
+except ImportError:
+    pass
+
+try:
+    # Setting page title and header
+    st.set_page_config(page_title="AI ChatBot", page_icon=":robot_face:")
+    st.markdown("<h1 style='text-align: center;'>AI ChatBot 😬</h1>",
+                unsafe_allow_html=True)
+
+    # Get environment variables
+    # openai.organization = os.environ['openai_organization']
+    # =======================================================
+    OPENAI_API_KEY = os.environ['openai_api_key']
+    pinecone_api_key = os.environ['pinecone_api_key']
+    pinecone_environment = os.environ['pinecone_environment']
+    openai.api_key = OPENAI_API_KEY
+    os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+    index_name = os.environ['index_name']
+    # ==================================================== #
+
+    # initialize connection to pinecone (get API key at app.pinecone.io)
+    pinecone.init(
+        api_key=pinecone_api_key,
+        environment=pinecone_environment  # find next to API key in console
+    )
+
+    # connect to index
+    index = pinecone.Index(index_name)
+    # wait a moment for the index to be fully initialized
+    time.sleep(1)
+    # stats = index.describe_index_stats()
+
+    # get openai api key from platform.openai.com
+    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY') or 'OPENAI_API_KEY'
+
+    model_name = 'text-embedding-ada-002'
+
+    embed = OpenAIEmbeddings(
+        model=model_name,
+        openai_api_key=OPENAI_API_KEY
+    )
+
+    text_field = "text"
+
+    # switch back to normal index for langchain
+    index = pinecone.Index(index_name)
+
+    vectorstore = Pinecone(
+        index, embed.embed_query, text_field
+    )
+
+    # Initialise session state variables
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = []
+    if 'past' not in st.session_state:
+        st.session_state['past'] = []
+    if 'messages' not in st.session_state:
+        st.session_state['messages'] = [
+            {"role": "system", "content": "You are a helpful assistant."}
+        ]
+    if 'model_name' not in st.session_state:
+        st.session_state['model_name'] = []
+
+    # Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
+    st.sidebar.title("Sidebar")
+    model_name = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
+    counter_placeholder = st.sidebar.empty()
+    # counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+    clear_button = st.sidebar.button("Clear Conversation", key="clear")
+
+    # Map model names to OpenAI model IDs
+    if model_name == "GPT-3.5":
+        model = "gpt-3.5-turbo-16k"
+    else:
+        model = "gpt-4"
+
+    # Initialize the large language model
+    llm = ChatOpenAI(
+        temperature=0,
+        openai_api_key=OPENAI_API_KEY,
+        model_name=model,
+    )
+
+    # reset everything
+    if clear_button:
+        st.session_state['generated'] = []
+        st.session_state['past'] = []
+        st.session_state['messages'] = [
+            {"role": "system", "content": "You are a helpful assistant."}
+        ]
+        st.session_state['model_name'] = []
+        # counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+
+    # generate a response
+
+    def generate_response(prompt):
+        query = prompt
+        st.session_state['messages'].append(
+            {"role": "user", "content": prompt})
+
+        ######################################################
+        memory = ConversationSummaryBufferMemory(
+            llm=llm, 
+            memory_key="chat_history", 
+            return_messages=True,
+            max_token_limit=16384,
+        )
+
+        chain = ConversationalRetrievalChain.from_llm(
+            llm,
+            retriever=vectorstore.as_retriever(search_kwargs={"k": 3}),
+            memory=memory,
+        )
+        response = chain.run({'question': query})
+        response = response.strip()
+        ######################################################
+        # response = ""
+        st.session_state['messages'].append(
+            {"role": "assistant", "content": response})
+
+        return response
+
+    # container for chat history
+    response_container = st.container()
+    # container for text box
+    container = st.container()
+
+    with container:
+        with st.form(key='my_form', clear_on_submit=True):
+            user_input = st.text_area("You:", key='input', height=100)
+            submit_button = st.form_submit_button(label='Send')
+
+        if submit_button and user_input:
+            output = generate_response(
+                user_input)
+            st.session_state['past'].append(user_input)
+            st.session_state['generated'].append(output)
+            st.session_state['model_name'].append(model_name)
+
+    if st.session_state['generated']:
+        with response_container:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state["past"][i],
+                        is_user=True, key=str(i) + '_user')
+                message(st.session_state["generated"][i], key=str(i))
+except Exception as e:
+    error_message = ''
+    # st.text('Hello World')
+    st.error('An error has occurred. Please try again.', icon="🚨")
+    # Just print(e) is cleaner and more likely what you want,
+    # but if you insist on printing message specifically whenever possible...
+    if hasattr(e, 'message'):
+        error_message = e.message
+    else:
+        error_message = e
+    st.error('ERROR MESSAGE: {}'.format(error_message))
